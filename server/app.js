48dcc08914c5d5478e7184a435e5a33c1e2625b7
Liron-Wassa <khruiuuxv@gmail.com>
2020-08-13 20:33:22 +0300

    heroku deployment

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ function flterCars(query) {
     return result;
 };
 
-if (process.env.NODE_ENV === "production" || true) {
+if (process.env.NODE_ENV === "production") {
     const root = path.join(__dirname, "..", 'client', 'build')
     app.use(express.static(root));
     app.get("*", (req, res) => {
