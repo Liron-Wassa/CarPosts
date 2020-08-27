4d53889660886c92ac42d1ab71b4edf699e4f4c9
Liron-Wassa <khruiuuxv@gmail.com>
2020-08-27 12:03:52 +0300

    try to deploy

--- server/utils/email.js
+++ server/utils/email.js
@@ -28,6 +28,8 @@ const checkConfirmTimeout = (req, res, email, time) => {
 const sendResetPasswordEmail = (req, res, email, token) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        host: 'https://car-posts.herokuapp.com',
+        port: 587,
         auth: {
             user: process.env.MY_EMAIL,
             pass: process.env.MY_PASSWORD
@@ -56,6 +58,8 @@ const sendResetPasswordEmail = (req, res, email, token) => {
 const sendConfirmAccountEmail = (req, res, email, token) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        host: 'https://car-posts.herokuapp.com',
+        port: 587,
         auth: {
             user: process.env.MY_EMAIL,
             pass: process.env.MY_PASSWORD
